8471908fb332f06ba447028a7cf134823eb89dfb
nabilAhmedN <nabilahmednahid@gmail.com>
2022-10-10 23:15:33 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './layouts/Main';
+import Home from './components/Home/Home';
+import Topics from './components/Topics/Topics';
+import Statistic from './components/Statistic/Statistic';
+import Blog from './components/Blog/Blog';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+
+        {
+          path: '/home',
+          element: <Home/>
+        },
+        {
+          path: '/topics',
+          element: <Topics/>
+        },
+        {
+          path: '/statistic',
+          element: <Statistic/>
+        },
+        {
+          path: '/blog',
+          element: <Blog/>
+        }
+      ]
+    }
+  ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/components/Statistic/Statistic.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Statistic = () => {
+    return (
+        <div>
+            <h3>This is Statistic</h3>
+        </div>
+    );
+};
+
+export default Statistic;
